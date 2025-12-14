726cc5fec38c7962e10976e9d278f6fa84e3a8d7
marko <markokstate@gmail.com>
2025-12-14 15:26:25 -0700

    Update site URL to www subdomain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://computerscienceguide.com',
+  site: 'https://www.computerscienceguide.com',
   integrations: [sitemap()],
   build: {
     assets: '_assets'
